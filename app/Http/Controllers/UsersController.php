6ff026def1ba7cfd6f605991468ea9fe2f372191
Hidran <hidran@gmail.com>
2020-01-23 01:07:01 +0100

    85.protects users routes

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,11 @@ use Illuminate\Support\Facades\Hash;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware ('auth:api', ['except'=>'login']);
+    }
+
     /**
      * Display a listing of the resource.
      *
